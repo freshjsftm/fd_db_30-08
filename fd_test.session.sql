5b77a3715769d3b73bf72ff07c1eaf6bc4dc57b1
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-09-01 15:25:57 +0300

    Add connect m:n

--- fd_test.session.sql
+++ fd_test.session.sql
@@ -8,8 +8,8 @@ DCL
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TABLE IF EXISTS "products_to_orders";
-DROP TABLE IF EXISTS "products";
 DROP TABLE IF EXISTS "orders";
+DROP TABLE IF EXISTS "products";
 DROP TABLE IF EXISTS "users";
 
 CREATE TABLE "users"(
@@ -21,42 +21,48 @@ CREATE TABLE "users"(
   "birthday" DATE NOT NULL CHECK("birthday"<current_date),
   "height" NUMERIC(3,2) NOT NULL CHECK("height">=1.1 AND "height"<=2.5)
 );
-CREATE TABLE "orders" (
-  "id" SERIAL PRIMARY KEY,--123
-  "userId" INT REFERENCES "users"("id"), --4
-  "createdAt" DATE NOT NULL CHECK("createdAt"<=current_date) DEFAULT current_date
-);
 CREATE TABLE "products" (
     "id" SERIAL PRIMARY KEY,
-    "title" VARCHAR(256) NOT NULL CHECK("title"!=''), 
+    "title" VARCHAR(256) NOT NULL CHECK("title"!='') UNIQUE, 
     "price" NUMERIC(5,2) NOT NULL CHECK("price">=0.01 AND "price"<=10000),
     "currency" VARCHAR(10) NOT NULL CHECK("currency"!=''),
-    "productionDate" DATE NOT NULL, 
+    "productionDate" DATE NOT NULL CHECK("productionDate"<current_date), 
     "amount" NUMERIC(4,0) CHECK("amount">=0 AND "amount"<=1000),
-    "expirationDate" VARCHAR(50),
-    UNIQUE("title"),
-    CONSTRAINT check_pr_date CHECK("productionDate"<current_date)
+    "expirationDate" VARCHAR(50)
+);
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "userId" INT REFERENCES "users"("id"),
+  "createdAt" DATE NOT NULL CHECK("createdAt"<=current_date) DEFAULT current_date
 );
 CREATE TABLE "products_to_orders"(
   "productId" INT REFERENCES "products"("id"), 
   "orderId" INT REFERENCES "orders"("id"),   
-  "amount" INT CHECK("amount">0),     
+  "quantity" INT CHECK("quantity">0),     
   PRIMARY KEY ("productId", "orderId")
 )
 
+INSERT INTO "users"("firstName","lastName","email","isMale","birthday","height")
+VALUES ('Rob', 'Bob', 'elon4@gmail.com', true, '1971-06-28', 1.88),
+('Fred', 'Bred', 'musk15@gmail.com', false, '1971-06-28', 1.48);
+
+INSERT INTO "products" ("title","price","currency","productionDate","amount")
+VALUES ('milk',32,'uh','2022-01-01',100),
+('egg',32,'uh','2022-01-01',100),
+('bread',13,'uh','2022-01-01',100),
+('qwerty',302,'uh','2022-01-01',100);
+
+INSERT INTO "orders"("userId") VALUES (1),(2),(1);
+
+INSERT INTO products_to_orders ("orderId", "productId", "quantity")
+VALUES (1,2,10),
+(1,1,1),
+(1,3,1),
+(2,3,5),
+(3,4,20),
+(3,2,10);
 
 
-
-
-
-
-
-INSERT INTO "products"
-VALUES ('bread', 12.5, 'UAH', '2022-05-30', 20, '1month'),
-('milk', 30.5, 'UAH', '2022-06-30', 5, '3month'),
-('meat', 100.55, 'UAH', '2022-07-22', 0, '2weeks'),
-('sugar', 22.5, 'UAH', '2022-08-21', NULL, '1year'),
-('eggs', 50, 'UAH', '2022-04-30', 300,  NULL);
 
 
 
@@ -73,15 +79,6 @@ CREATE TABLE "messages"(
 
 INSERT INTO "messages"("author","body")
 VALUES ('Elon Musk','hi');
-
-
-
-
-INSERT INTO "users"("firstName","lastName","email","isMale","birthday","height")
-VALUES ('Tom', 'Musk', 'elon@gmail.com', true, '1971-06-28', 1.88),
-('Elen', 'Musk', 'musk1@gmail.com', false, '1971-06-28', 1.48),
-('Elon', 'Musk', 'musk2@gmail.com', true, '1971-06-28', 1.84),
-('Elon', 'Musk', 'musk3@gmail.com', false, '1971-06-28', 1.81);
 
 ALTER TABLE "users"
 DROP CONSTRAINT "users_email_key";
