ce11ef5d6ff6b2e67fae10af72bdc5d81e4d3fe8
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-09-02 15:06:20 +0300

    Create randomUser

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 const { Client } = require("pg");
-
+const axios = require("axios");
+const http = axios.create({
+  baseURL: 'https://randomuser.me/api/'
+});
+const loadUser = async()=>{
+  const {data:{results}} = await http.get('?results=100&seed=fd2022&nat=us,dk,fr,gb&page=3');
+  return results;
+}
 const config = {
-  user: 'postgres', 
-  password: 'postgres', 
-  host: 'localhost', 
-  database: 'fd_test', 
-  port: 5432
-}
-const users = [{
-  firstName:"Jan1",
-  lastName:"Janskyi",
-  email:"qwerty1@qwe.com",
-  isMale: true,
-  birthday:'1971-06-28',
-  height:1.78
-},{
-  firstName:"Jan2",
-  lastName:"Janskyi",
-  email:"qwerty2@qwe.com",
-  isMale: true,
-  birthday:'1971-06-28',
-  height:1.78
-}]
-
-function mapUsers(users){
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  database: "fd_test",
+  port: 5432,
+};
+function mapUsers(users) {
   return users
-          .map((user)=>`('${user.firstName}', '${user.lastName}', '${user.email}', '${user.isMale}', '${user.birthday}','${user.height}')`)
-          .join(',')
+    .map(
+      ({name:{first, last}, email, gender, dob:{date}}) =>
+        `('${first}', '${last}', '${email}', '${gender==='male'}', '${date}','${(Math.random()+1.2).toFixed(2)}')`
+    )
+    .join(",");
 }
-
 const client = new Client(config);
 start();
-
-async function start(){
+async function start() {
   await client.connect();
+  const users = await loadUser();
   const res = await client.query(`
   INSERT INTO "users"("firstName","lastName","email","isMale","birthday","height")
   VALUES ${mapUsers(users)};
